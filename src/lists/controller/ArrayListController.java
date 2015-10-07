468975d03fbbb7997fcf6bf0038e64cd6094e280
josiahelmer <jelm9149@740s-pl-112.local>
2015-10-07 13:56:40 -0600

    Stuff

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,18 +1,31 @@
 package lists.controller;
 
 import java.util.ArrayList;
+import lists.view.ListViewer;
 
 public class ArrayListController
 {
 	private ArrayList<String> firstWords;
-	
+	private ListViewer popupDisplay;
+
 	public ArrayListController()
 	{
 		firstWords = new ArrayList<String>();
+		popupDisplay = new ListViewer();
 	}
 	
 	public void start()
 	{
 		firstWords.add("ladksfgjldsfjkglo");
+		firstWords.add("bababababababababababababab");
+		displayList();
+	}
+	
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The contents at " + spot+ " are " + firstWords.get(spot));
+		}
 	}
 }
