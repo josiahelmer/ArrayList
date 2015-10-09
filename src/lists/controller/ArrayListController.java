7943379ea359eb36d7a9f03c9f58cd78906c8bb4
josiahelmer <jelm9149@740s-pl-112.local>
2015-10-09 13:53:57 -0600

    popup

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -18,14 +18,30 @@ public class ArrayListController
 	{
 		firstWords.add("ladksfgjldsfjkglo");
 		firstWords.add("bababababababababababababab");
+		firstWords.add(1,"CRYING INSANELY");
+		
 		displayList();
+		firstWords.set(2,"mamamamamamamamamamama");
+		firstWords.remove(0);
+		displayList();
+		showOtherLoop();
+
 	}
 	
 	private void displayList()
 	{
+		popupDisplay.showList("The size of the list is " + firstWords.size());
 		for(int spot = 0; spot < firstWords.size(); spot++)
 		{
 			popupDisplay.showList("The contents at " + spot+ " are " + firstWords.get(spot));
+		}
+	}
+	
+	private void showOtherLoop()
+	{
+		for(String words : firstWords)
+		{
+			popupDisplay.showList(words);
 		}
 	}
 }
